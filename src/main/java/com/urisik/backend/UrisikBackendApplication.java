ad9a183d92ffccf0b14d6c6f80f6e076db450238
wonjun-lee-fcwj245 <fcwj24@gail.com>
2026-01-01 21:15:50 +0900

    chore: init spring boot project

---
+++ src/main/java/com/urisik/backend/UrisikBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urisik.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrisikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrisikBackendApplication.class, args);
+	}
+
+}
