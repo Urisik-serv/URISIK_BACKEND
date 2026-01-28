5955f92db3bad2dac6adb4db106aa77bfb6e550f
GitHub 핸들명 <GitHub >
2026-01-29 05:03:47 +0900

    refactor : 리뷰 중복 작성 제한 로직 추가

--- src/main/java/com/urisik/backend/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/urisik/backend/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,12 @@
 package com.urisik.backend.domain.review.repository;
 
+import com.urisik.backend.domain.member.entity.FamilyMemberProfile;
+import com.urisik.backend.domain.recipe.entity.Recipe;
 import com.urisik.backend.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    boolean existsByFamilyMemberProfileAndRecipe(FamilyMemberProfile profile, Recipe recipe);
 
 }
