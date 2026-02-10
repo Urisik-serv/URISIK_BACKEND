eec11a369ca2814e17dc091149c9b7612ad326c4
wonjun-lee-fcwj245 <fcwj24@gail.com>
2026-02-10 18:58:09 +0900

    .

--- src/main/java/com/urisik/backend/global/auth/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/urisik/backend/global/auth/oauth2/CustomSuccessHandler.java
@@ -34,7 +34,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         addRefreshCookie(response, refreshToken);
 
         // 프론트 특정 url 로컬에서 : "http://localhost:5173/login/callback"
-        response.sendRedirect("http://localhost:5173/login/callback");
+        response.sendRedirect("https://urisik.vercel.app/login/callback");
     }
     // 배포 에선 "https://urisik.vercel.app/login/callback"
 
