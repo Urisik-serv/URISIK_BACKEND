af17d7864c3b3df1242c912dc17724dc864ab116
dev.geonwoo_ <woo6629058@catholic.ac.kr>
2026-02-10 14:29:58 +0900

    chore: fix family role comments

--- src/main/java/com/urisik/backend/domain/member/service/FamilyMemberProfileService.java
+++ src/main/java/com/urisik/backend/domain/member/service/FamilyMemberProfileService.java
@@ -39,7 +39,6 @@ public class FamilyMemberProfileService {
     private final S3Uploader s3Uploader;
     private final S3Remover s3Remover;
     private final AllergenAlternativeRepository allergenAlternativeRepository;
-    //
 
     //post
     @Transactional
@@ -71,7 +70,7 @@ public class FamilyMemberProfileService {
         FamilyRole requestedRole = req.getRole();
 
 
-        // 엄마/아빠만 중복 제한
+        // 엄마/아빠/할머니/할아버지 중복 제한
         if (requestedRole == FamilyRole.MOM || requestedRole == FamilyRole.DAD
                 || requestedRole == FamilyRole.GRANDMOTHER || requestedRole == FamilyRole.GRANDFATHER ) {
 
@@ -150,7 +149,7 @@ public class FamilyMemberProfileService {
         // 3) role 변경 + (MOM/DAD) 중복 체크
         if (req.getRole() != null) {
             FamilyRole newRole = req.getRole();
-//FamilyRole.GRANDPA
+
             if (newRole == FamilyRole.MOM || newRole == FamilyRole.DAD
                     || newRole == FamilyRole.GRANDMOTHER  || newRole == FamilyRole.GRANDFATHER  ) {
                 boolean alreadyExists = familyMemberProfileRepository
